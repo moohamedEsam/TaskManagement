bc537add598ecf51ad1805067c09e62187566b64
mohamed esam <moohamedEsam0@gmail.com>
2022-08-12 14:45:57 +0200

    projects screen

--- app/src/main/java/com/example/taskmanagement/presentation/screens/projects/ProjectsViewModel.kt
+++ app/src/main/java/com/example/taskmanagement/presentation/screens/projects/ProjectsViewModel.kt
@@ -5,17 +5,41 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.taskmanagement.domain.dataModels.project.Project
 import com.example.taskmanagement.domain.dataModels.utils.Resource
+import com.example.taskmanagement.domain.dataModels.utils.SnackBarEvent
 import com.example.taskmanagement.domain.repository.MainRepository
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 
 class ProjectsViewModel(private val repository: MainRepository) : ViewModel() {
-    val projects = mutableStateOf<Resource<List<Project>>>(Resource.Initialized())
+    private val projects = mutableStateOf<Resource<List<Project>>>(Resource.Initialized())
+    val filteredProjects = mutableStateOf(emptyList<Project>())
+    private val snackBarChannel = Channel<SnackBarEvent>()
+    val receiveChannel = snackBarChannel.receiveAsFlow()
 
     init {
         getProjects()
     }
 
-    fun getProjects() = viewModelScope.launch {
-        projects.value = repository.getUserProjects()
+    private fun getProjects() {
+        viewModelScope.launch {
+            projects.value = repository.getUserProjects()
+            projects.value.onSuccess {
+                filteredProjects.value = it
+            }
+            projects.value.onError {
+                val event = SnackBarEvent(it ?: "") { getProjects() }
+                snackBarChannel.send(event)
+            }
+        }
     }
+
+    fun filter(query: String) {
+        projects.value.onSuccess { projectList ->
+            filteredProjects.value =
+                projectList.filter { it.name.contains(query) || it.description.contains(query) }
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/taskmanagement/presentation/screens/projects/projectsScreen.kt
+++ app/src/main/java/com/example/taskmanagement/presentation/screens/projects/projectsScreen.kt
@@ -5,19 +5,24 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.taskmanagement.R
 import com.example.taskmanagement.domain.dataModels.project.Project
-import com.example.taskmanagement.presentation.customComponents.HandleResourceChange
+import com.example.taskmanagement.presentation.customComponents.*
 import com.example.taskmanagement.presentation.navigation.Screens
+import kotlinx.coroutines.flow.collectLatest
 import org.koin.androidx.compose.inject
+
+const val ratio = 4
 
 @Composable
 fun ProjectsScreen(
@@ -25,65 +30,99 @@ fun ProjectsScreen(
     snackbarHostState: SnackbarHostState
 ) {
     val viewModel: ProjectsViewModel by inject()
-    val projects by viewModel.projects
-    HandleResourceChange(
-        resource = projects,
-        onSuccess = {},
-        snackbarHostState = snackbarHostState,
-        onSnackbarClick = { viewModel.getProjects() }
-    )
-    projects.onSuccess {
-        ProjectsScreenContent(
-            projects = it,
-            navHostController = navHostController
-        )
+    val channel = viewModel.receiveChannel
+    val projects by viewModel.filteredProjects
+    LaunchedEffect(key1 = Unit) {
+        channel.collectLatest {
+            handleSnackBarEvent(it, snackbarHostState)
+        }
     }
+    ProjectsScreenContent(
+        projects = projects,
+        viewModel = viewModel,
+        navHostController = navHostController
+    )
+}
+
+
+@Composable
+fun FilterProjectTextField(viewModel: ProjectsViewModel) {
+    var value by remember {
+        mutableStateOf("")
+    }
+    TextField(
+        value = value,
+        onValueChange = {
+            value = it
+            viewModel.filter(it)
+        },
+        label = { Text(text = "search projects") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    )
 }
 
 @Composable
-fun ProjectsScreenContent(projects: List<Project>, navHostController: NavHostController) {
-    LazyVerticalGrid(columns = GridCells.Adaptive(120.dp)) {
-        items(projects) { project ->
-            ProjectItem(
-                project = project,
-                navHostController = navHostController
-            )
+fun ProjectsScreenContent(
+    projects: List<Project>,
+    viewModel: ProjectsViewModel,
+    navHostController: NavHostController
+) {
+    Column {
+        FilterProjectTextField(viewModel = viewModel)
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            item {
+                OutlinedCenteredCard(
+                    modifier = Modifier.fillMaxHeight(ratio),
+                    onClick = { navHostController.navigate(Screens.ProjectForm.withArgs(" ", " ")) }
+                ) {
+                    Column {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = null,
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        )
+                        Text(text = "New Project")
+                    }
+                }
+            }
+            items(projects) { project ->
+                ProjectItem(
+                    project = project,
+                    navHostController = navHostController
+                )
+            }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProjectItem(project: Project, navHostController: NavHostController) {
-    Card(
+    ElevatedCenteredCard(
         modifier = Modifier
-            .padding(8.dp)
+            .fillMaxHeight(ratio)
             .clickable {
                 navHostController.navigate("${Screens.Project.route}/${project.id}")
             }
     ) {
         Column(modifier = Modifier.padding(8.dp)) {
-            Text(text = project.name, style = MaterialTheme.typography.headlineMedium)
+            Text(
+                text = project.name,
+                style = MaterialTheme.typography.headlineMedium,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = project.description,
                 style = MaterialTheme.typography.bodyMedium,
                 color = MaterialTheme.colorScheme.onPrimaryContainer
             )
-            Spacer(modifier = Modifier.height(8.dp))
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Icon(
-                    painter = painterResource(id = R.drawable.team),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    project.members.count().toString(),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onPrimaryContainer
-                )
-            }
         }
     }
 }
